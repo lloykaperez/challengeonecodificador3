cbcb4084b803c48740cacc89d1af80199e4b5f36
Lloyka Pérez <105314188+lloykaperez@users.noreply.github.com>
2022-08-31 19:55:40 -0400

    Primera estructura del proyecto

---
+++ script.js
@@ -0,0 +1,22 @@
+const textoIngresado = document.querySelector(".texto-ingresado");
+const btnCodificador = document.querySelector(".btn-codigicador");
+const btnDecodificador = document.querySelector(".btn-decodificador");
+const textoCopiado = document.querySelector(".texto-copiado");
+const btnCopiar = document.querySelector(".copiar");
+
+//Codificador
+
+btnCodificador.onclick = codificar;
+
+function codificador(string) {
+    
+    alert(textoIngresado.value)
+}
+
+//Decodificador
+
+//Copiar
+
+
+
+
